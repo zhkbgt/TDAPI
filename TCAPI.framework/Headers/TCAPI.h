293d2e8fa90ef144865f2651160c84dbb5b616f4
zh神夕 <kkbbggtt@163.com>
2019-03-27 16:44:05 +0800

    add large file

---
+++ TCAPI.framework/Headers/TCAPI.h
@@ -0,0 +1,35 @@
+//
+//  TCAPI.h
+//  TCAPI
+//
+//  Created by 张兵 on 2019/3/26.
+//  Copyright © 2019 张兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TCAPI.
+FOUNDATION_EXPORT double TCAPIVersionNumber;
+
+//! Project version string for TCAPI.
+FOUNDATION_EXPORT const unsigned char TCAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCAPI/PublicHeader.h>
+
+#import <TCAPI/APIWindowContainer.h>
+#import <TCAPI/APIWidgetContainer.h>
+#import <TCAPI/APIManager.h>
+#import <TCAPI/APIEvent.h>
+#import <TCAPI/APIUpdateManager.h>
+#import <TCAPI/APIWebView.h>
+#import <TCAPI/APIScriptMessage.h>
+#import <TCAPI/APIModuleMethod.h>
+
+#import <TCAPI/UZAppDelegate.h>
+#import <TCAPI/UZWidget.h>
+#import <TCAPI/UZAppUtils.h>
+#import <TCAPI/UZModule.h>
+#import <TCAPI/APIWebView.h>
+#import <TCAPI/NSDictionaryUtils.h>
+#import <TCAPI/UZModule.h>
+
